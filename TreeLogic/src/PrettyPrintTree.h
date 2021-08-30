4eed5a2b3cd4913677f536df9d5d6d52a15e0030
Fixedpl <marcinplewik@gmail.com>
2021-08-31 00:02:01 +0200

    Added comments to PrettyPrintTree

--- TreeLogic/src/PrettyPrintTree.h
+++ TreeLogic/src/PrettyPrintTree.h
@@ -48,6 +48,21 @@ void PrettyPrintTree<T>::buildTreeSkeleton(INode<T>* root_node,
 										   const uint32_t& level,
 										   const FatherDirection& father_direction)
 {
+	/****************************************************************
+	*	buildTreeSkeleton function builds this:
+	*
+	*			  0
+	*		   +- + -+
+	*		   1	 3
+	*		+- +
+	*		2
+	*
+	*	There are lines missing which are filled with fillMissingLines
+	* 
+	*	Divides possibly many sons into left sons and right sons
+	*	and then traverse the tree with In Order performing
+	*	actions on each node. Basically rendering tree from left to right
+	* ***************************************************************/
 	std::vector<INode<T>*> sons = root_node->getSons();
 	uint32_t sons_count = root_node->sonsCount();
 	uint32_t left_sons  = (sons_count + 1) / 2;
@@ -56,6 +71,17 @@ void PrettyPrintTree<T>::buildTreeSkeleton(INode<T>* root_node,
 		buildTreeSkeleton(sons[i], tree_view, size, level + 2, FatherDirection::LEFT);
 	}
 
+	/****************************************************************
+	*	It renders leftmost node and adds spacing on every other node
+	*	Each node has 1 spacing on each side
+	* 
+	*	root_node_id_length is needed to know how many lines
+	*	to render relating to id length e.g. left needs more spacing:
+	* 
+	*	+-----+					+--+
+	*	1000  1					2  1
+	* 
+	* ***************************************************************/
 	uint32_t root_node_id_length = std::to_string(root_node->getId()).size();
 	for (int i = 0; i < size; ++i) {
 		if (i == level) {
@@ -66,6 +92,9 @@ void PrettyPrintTree<T>::buildTreeSkeleton(INode<T>* root_node,
 		}
 	}
 
+
+	// If its not topmost most node adds + above and - on appropriate side
+	// - here is needed for filling lines later
 	if (level != 0) {
 		tree_view[level - 1][tree_view[level - 1].size() - 2] = '+';
 		if (father_direction == FatherDirection::LEFT) {
@@ -75,9 +104,11 @@ void PrettyPrintTree<T>::buildTreeSkeleton(INode<T>* root_node,
 			tree_view[level - 1][tree_view[level - 1].size() - 3] = '-';
 		}
 	}
+	// If its not botmost node adds + below it
 	if (level + 1 < size && sons_count > 0) {
 		tree_view[level + 1][tree_view[level + 1].size() - 2] = '+';
 	}
+
 
 	for (int i = left_sons; i < sons_count; ++i) {
 		buildTreeSkeleton(sons[i], tree_view, size, level + 2, FatherDirection::RIGHT);
@@ -87,6 +118,16 @@ void PrettyPrintTree<T>::buildTreeSkeleton(INode<T>* root_node,
 template <typename T>
 void PrettyPrintTree<T>::fillMissingLines(std::string* tree_view, const uint32_t& size)
 {
+	/*****************************************
+	*	It turns patterns like this:
+	* 
+	*	+-		+		+		-+
+	* 
+	*	into this:
+	* 
+	*	+-------+		+--------+
+	* 
+	****************************************/
 	for (int i = 1; i < size; i += 2) {
 		bool lock = false;
 		for (int j = 0; j < tree_view[i].size() - 1; ++j) {
@@ -131,6 +172,19 @@ uint32_t PrettyPrintTree<T>::findDiameter(INode<T>* root_node, const uint32_t& l
 template <typename T>
 void PrettyPrintTree<T>::print(INode<T>* root_node)
 {
+	/********************************************************************
+	*   First it finds tree diameter to know how many levels
+	*   of string to create. Since tree looks like this in console:
+	*
+	*		  0
+	*	   +--+--+
+	*      1	 3
+	*   +--+
+	*   2
+	*	
+	*	it needs to reserve 2 * tree_diameter - 1 strings to be filled. 
+	********************************************************************/
+
 	uint32_t tree_diameter = 2 * findDiameter(root_node) - 1;
 	std::string* tree_view_to_fill = new std::string[tree_diameter];
 
